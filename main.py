486aa5751711cac8ab0a8f13b1ea8c8e12cb5a0a
AnthillVG <vladf2@gmail.com>
2022-01-02 13:42:08 +0300

    passenger input for Passenger class adjusted yes/no/incorrect check adjusted. incorrect answer loop added floor input added

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 from Passenger import Passenger
 
 
+passenger_names = []
+passenger_weights = []
+floors = []
+
+
 def input_passenger():
-    name = input('Input the passengers name: ')
-    weight = input('Input the passengers weight: ')
-    person = Passenger(name, float(weight))
+    Passenger.name = input('Input the passengers name: ')
+    Passenger.weight = input('Input the passengers weight: ')
+    # person = Passenger(name, float(weight))
+    passenger_names.append(Passenger.name)
+    passenger_weights.append(Passenger.weight)
 
 
 def check_input(answer):
-    if answer == 'Yes' or answer == 'No':
+    if answer == 'yes' or answer == 'no':
         return True
     return False
 
 
 next_pas_check = True
-
 while next_pas_check:
     input_passenger()
-    check = input('Would you like to add more passengers? (Yes/No): ')
+    check = input('Would you like to add a passenger? (Yes/No): ')
+    check = check.lower()
     if not check_input(check):
-        print('Incorrect answer')
-        continue
-    if check == 'No':
+        while not check_input(check):
+            check = input('Incorrect answer. Please, try again: ')
+    if check == 'no':
         next_pas_check = False
 
 
 def weight_count():
-    pass
+    for i in range(0, len(passenger_weights)):
+        pass
 
 
 def floor_input():
-    pass
+    for i in range(0, len(passenger_names)):
+        floor = input('What floor does passenger ' + passenger_names[i] + ' go to? ')
+        floors.append(floor)
+
+
+floor_input()
+print(passenger_names)
+print(passenger_weights)
+print(floors)
 
 
 # person = Passenger('Oleg', 14)
